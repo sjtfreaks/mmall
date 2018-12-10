8ed8036ccb9774cb3dff07c1eb494c1ad17a4cfb
sjtfreaks <13028911306@163.com>
2018-12-10 16:41:56 +0800

    获取用户登录信息
    提示问题与答案

--- src/main/java/com/mmall/service/impl/UserServiceimol.java
+++ src/main/java/com/mmall/service/impl/UserServiceimol.java
@@ -2,6 +2,7 @@ package com.mmall.service.impl;
 
 import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
+import com.mmall.common.TokenCache;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
@@ -9,6 +10,8 @@ import com.mmall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * Created by jet on 2018-12-09.
@@ -77,5 +80,31 @@ public class UserServiceimol implements IUserService{
             return ServerResponse.createByErrorMessage("参数错误");
         }
         return ServerResponse.createBySuccessMessage("校验成功");
+    }
+    public ServerResponse selectQuestion(String username){
+        ServerResponse validResponse = this.checkValid(username,Const.USERNAME);
+
+        if (validResponse.isSuccess()){
+            //用户不存在
+            return ServerResponse.createByErrorMessage("用户不存在");
+        }
+        String qusetion = userMapper.selectQuestionByUsername(username);
+        if (org.apache.commons.lang3.StringUtils.isNotBlank(qusetion)){
+            return ServerResponse.createBySuccess(qusetion);
+        }
+        return ServerResponse.createByErrorMessage("找回密码的问题为空");
+
+    }
+
+    public ServerResponse<String> checkAnswer(String username,String question,String answer) {
+        int resultCount = userMapper.checkAnswer(username, question, answer);
+        if (resultCount > 0) {
+            //说明问题是这个用户的
+            String forgetToken = UUID.randomUUID().toString();
+            TokenCache.setKey("token_" + username, forgetToken);
+            return ServerResponse.createBySuccess(forgetToken);
+
+        }
+        return ServerResponse.createByErrorMessage("问题的答案错误");
     }
 }
